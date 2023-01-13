82863c86c17e844b218d77ae59df3afc04646642
Shadirvan <shadirvan2021@gmail.com>
2023-01-13 22:17:50 +0530

    Added Few Theeme Data

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Pudget',
+      theme: ThemeData(primarySwatch: Colors.red, accentColor: Colors.amber),
       home: MyHomePage(),
     );
   }
@@ -69,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Pudget'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
