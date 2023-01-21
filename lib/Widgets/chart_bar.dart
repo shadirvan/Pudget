ad75f241799577b6ceacfff5c955f636ad33a072
Shadirvan <shadirvan2021@gmail.com>
2023-01-21 11:22:40 +0530

    set card bar height using LayoutBuilder

--- lib/Widgets/chart_bar.dart
+++ lib/Widgets/chart_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ChartBar extends StatelessWidget {
   final String label;
@@ -9,42 +10,51 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            height: 20,
-            child: FittedBox(
-                child: Text('₹${spendingAmount.toStringAsFixed(0)}'))),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(children: [
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Column(
+          children: [
             Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1.0),
-                color: Color.fromRGBO(220, 220, 220, 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
+                height: constraints.maxHeight * 0.15,
+                child: FittedBox(
+                    child: Text('₹${spendingAmount.toStringAsFixed(0)}'))),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
             ),
-            FractionallySizedBox(
-              heightFactor: spendingPctOfTotal,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(10),
+            Container(
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
+                FractionallySizedBox(
+                  heightFactor: spendingPctOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(label),
               ),
             ),
-          ]),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(label),
-      ],
+          ],
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -121,13 +121,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
                         MediaQuery.of(context).padding.top) *
-                    0.25,
+                    0.3,
                 child: Chart(_recentTransactions)),
             Container(
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
                         MediaQuery.of(context).padding.top) *
-                    0.75,
+                    0.70,
                 child: TransactionList(_userTransactions, _deleteTransaction)),
           ],
         ),
